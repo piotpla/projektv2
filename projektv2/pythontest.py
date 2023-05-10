f518d274a7831397a38026201d3683292a4aec55
piotpla <56163255+piotpla@users.noreply.github.com>
2023-05-10 13:39:14 +0200

    Create pythontest.py

---
+++ projektv2/pythontest.py
@@ -0,0 +1,18 @@
+import time 
+from common.methods import set_progress 
+start = time.time() 
+def Fibonacci(n): 
+	if n < 0: 
+		print("Incorrect input") 
+	elif n == 0: 
+		return 0 
+	elif n == 1 or n == 2: 
+		return 1 
+	else: 
+		return Fibonacci(n-1) + Fibonacci(n-2) 
+result_fib=Fibonacci(40) 
+print(result_fib) 
+set_progress(result_fib) 
+end=time.time() 
+total_time=end-start print(total_time) 
+set_progress(total_time)
